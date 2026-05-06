6b39fa85166a1129563424b317d725cfdc87bb1a
unknown <Email@example.com>
2026-05-06 19:31:27 +0530

    fixed json

--- src/components/Pagess/Product.jsx
+++ src/components/Pagess/Product.jsx
@@ -60,7 +60,8 @@ function AddProduct() {
       image
     };
 
-    axios.post("https://smartshop-api-oas7.onrender.com/product", newProduct)
+    axios.post
+    ("https://smartshop-api-oas7.onrender.com/product", newProduct)
       .then(() => {
        toast.success("Product added successfully!",{
         autoClose:800
